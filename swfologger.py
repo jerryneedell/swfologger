4af1637e95ba322239a00543e2adb788d2db9c44
jerryneedell <jerryneedell@gmail.com>
2021-06-09 09:50:58 +0000

    execute from /usr/local/bin -- store data in ~/logger/

--- swfologger.py
+++ swfologger.py
@@ -4,33 +4,32 @@ import os
 
 ser = serial.Serial('/dev/serial0', 9600, timeout=.1)
 
-
 while True:
-    #data = ser.read(ser.inWaiting())
     data = ser.read_until(expected=b'\n')
     if data:
         if data[len(data)-1] != 0x0a :
             print("newline terminator missing\rn")
-        print([hex(x) for x in data])
-        print(data)
+        #print([hex(x) for x in data])
+        #print(data)
         try:
-            with open("swfolog.txt","a") as logfile:
+            with open("/home/pi/logger/swfolog.txt","a") as logfile:
                 logfile.write(data.decode("UTF-8"))
         except UnicodeDecodeError as e:
             print("decode error",e)
         if data[0] == 0x52 and data[1] == 0x50:  # RP
             print("Playback request\r\n")
-            if not os.path.exists("swfoplayback.txt"):
-                os.rename("swfolog.txt","swfoplayback.txt")
-            os.system("python3 swfoplayback.py &")
-        if data[0] == 0x53 and data[1] == 0x44:  # SD
-            print("Shutdown request\r\n")
-            os.system("sudo shutdown -h now ")
+            if not os.path.exists("/home/pi/logger/swfoplayback.txt"):
+                os.rename("/home/pi/logger/swfolog.txt","/home/pi/logger/swfoplayback.txt")
+            os.system("python3 /usr/local/bin/swfoplayback.py &")
         elif data[0] == 0x44 and data[1] == 0x50:  # DP
-            if os.path.exists("swfoplayback.txt"):
+            if os.path.exists("/home/pi/logger/swfoplayback.txt"):
                 print("Deleted Playback File\r\n")
-                os.remove("swfoplayback.txt")
+                os.remove("/home/pi/logger/swfoplayback.txt")
             else:
                 print("No Playback File found\r\n")
+        elif data[0] == 0x53 and data[1] == 0x44:  # SD
+            print("Shutdown request\r\n")
+            os.system("sudo shutdown -h now ")
+            break
     else:
         print(".",end = '', flush = True)
--- swfoplayback.py
+++ swfoplayback.py
@@ -5,13 +5,13 @@ import os
 ser = serial.Serial('/dev/serial0', 9600, timeout=1)
 
 
-with open("swfoplayback.txt","r") as playbackfile:
+with open("/home/pi/logger/swfoplayback.txt","r") as playbackfile:
     dumping_data = True
     while dumping_data:
         line=playbackfile.readline()
         if line:
             ser.write(bytes(line,"UTF-8"))
-            print(line)
+            #print(line)
         else:
             dumping_data = False
             print("End of Playback")
